3f6552ff19e980e06f67625bbe38a9d4f03985bb
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-10-22 23:02:47 -0400

    add contact component

---
+++ my_website/src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contact = (props) => {
+  return(
+    <div className="contact_page">
+          <div className="contact_heading">
+            <h1>Contact Details</h1>
+          </div>
+          <div className="contact_info">
+            <p><span class="email">Email Address: </span> adeseyesijuwade@gmail.com</p>
+            <p><span class="phone">Phone Number: </span> +1(732)789 3916</p>
+            <p><span class="phone">Location: </span>New York, New York</p>
+          </div>
+    </div>
+  )
+}
+
+export default Contact;
