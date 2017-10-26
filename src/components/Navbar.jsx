08dfcc92d4c4d91c5dbd7935bb76c32b4234e0b7
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-10-26 14:57:48 -0400

    hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = (props) => {
   return(
     <div className="navbar">
           <div className="nav">
-            <a href="" className="menu-icon"></a>
+            <a href='#' className="menu-icon"></a>
             <ul>
               <h2>My Portfolio</h2>
               <div className="links">
