0c1375d9e511eb90d71f05184c8fedeecbf6af8a
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-10-27 03:28:58 -0400

    edit about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = (props) => {
           <div className="about_info">
             <div className="profpic"></div>
             <div className="content">
-              <p>I am Seye - born in London, raised in Nigeria, currently living in New York, NY. I am a junior web developer with a background in data analysis. I am highly familiar with a variety of programming languages including JavaScript, React.js, Express.js, Ruby on Rails and database technologies including SQL. I love building and designing websites and rich interactive web applications.</p>
+              <p>I am Seye - born in London, raised in Nigeria, currently living in New York. I am a junior web developer with a background in data analysis. I am highly familiar with a variety of programming languages including JavaScript, React.js, Express.js, Ruby on Rails and database technologies including SQL. I love building and designing websites and rich interactive web applications.</p>
               <p>I pride myself on being friendly, approachable, patient and eager to learn. If you are looking for a goal oriented strategist and experienced developer, contact me through the link below. I look forward to working with <span className="you">YOU</span>.</p>
             </div>
           </div>
