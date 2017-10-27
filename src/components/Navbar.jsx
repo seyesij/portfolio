c93fca4660d543dee6c5875249ac903e1c465a68
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-10-27 03:02:09 -0400

    add resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = (props) => {
             <ul>
               <h2>My Portfolio</h2>
               <div className="links">
-              <li><a href="https://www.linkedin.com/in/seyesijuwade/" target="_blank" rel="noopener noreferrer" className="navlink"><h3>linkedin</h3></a></li>
+              <li><a href="https://drive.google.com/file/d/0B3wNsmEz0hqtWmNCQUE2OWlLeVk/view" target="_blank" rel="noopener noreferrer" className="navlink"><h3>resume</h3></a></li>
               <li><a href="https://github.com/seyesij" target="_blank" rel="noopener noreferrer" className="navlink"><h3>github</h3></a></li>
               <li><NavLink className="navlink" exact to='/contact'><h3>contact</h3></NavLink></li>
               <li><NavLink className="navlink" exact to='/about'><h3>about</h3></NavLink></li>
