29598b42fdcf3eb97b9fbbcad2a760136989b22b
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-10-27 17:49:06 -0400

    add skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,14 @@ const About = (props) => {
           <div className="about_info">
             <div className="profpic"></div>
             <div className="content">
-              <p>I am Seye - born in London, raised in Nigeria, currently living in New York. I am a junior web developer with a background in data analysis. I am highly familiar with a variety of programming languages including JavaScript, React.js, Express.js, Ruby on Rails and database technologies including SQL. I love building and designing websites and rich interactive web applications.</p>
-              <p>I pride myself on being friendly, approachable, patient and eager to learn. If you are looking for a goal oriented strategist and experienced developer, contact me through the link below. I look forward to working with <span className="you">YOU</span>.</p>
+              <p>I am Seye - born in London, raised in Nigeria, currently living in New York. I am a junior web developer with a background in data analysis. I am highly familiar with a variety of programming languages and database technologies. I love building and designing websites and rich interactive web applications.</p>
+              <p>I pride myself on being friendly, approachable, patient and eager to learn. If you are looking for a goal oriented strategist and experienced developer, contact me through the link below. <br></br><br></br>I look forward to working with <span className="you">YOU</span>.</p>
             </div>
           </div>
+          <div className="skills">
+              <h2>Technical Skills</h2>
+              <p>HTML5, CSS3, jQuery, JavaScript, React.js, Node.js, Express.js, Ruby, Rails, Google Firebase, PostgreSQL, MySQL, Python, Postman, RESTful API, Git, Github</p>
+            </div>
           <div className="footer">
             <Link to='/contact'>
               <button>Contact Me</button>
