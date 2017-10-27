a0c5bc85acf925ef317a601a76f6c3db4797c224
adeseye sijuwade <adeseyesijuwade@gmail.com>
2017-10-27 03:02:35 -0400

    add linkedin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,10 @@ const Contact = (props) => {
             <h1>Contact Details</h1>
           </div>
           <div className="contact_info">
-            <p><span className="info">Email Address: </span> adeseyesijuwade@gmail.com</p>
-            <p><span className="info">Phone Number: </span> +1(732)789 3916</p>
-            <p><span className="info">Location: </span>New York, New York</p>
+            <p className="linkedin"><span className="info">LinkedIn: </span><a href="https://www.linkedin.com/in/seyesijuwade/" target="_blank" rel="noopener noreferrer" className="link"><h4>https://www.linkedin.com/in/seyesijuwade</h4></a></p>
+            <p className="other"><span className="info">Email Address: </span> adeseyesijuwade@gmail.com</p>
+            <p className="other"><span className="info">Phone Number: </span> +1(732)789 3916</p>
+            <p className="other"><span className="info">Location: </span>New York, NY</p>
           </div>
           <div className="contact_footer">
             <p>Copyright © 2017. Website by Adeseye Sijuwade.</p>
